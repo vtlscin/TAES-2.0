c0a259cc7d258d261fae1c9e4f95eaaaee0a5ac8
Vinicius Thiago <vtls@cin.ufpe.br>
2019-11-22 11:16:45 -0300

    alteraçao da classe de conflitos

--- src/blocoEstatico/classeParaConflito.java
+++ src/blocoEstatico/classeParaConflito.java
@@ -2,9 +2,14 @@ package blocoEstatico;
 
 public class classeParaConflito {
 
+	static String x;
+	static String y;
 	static 
 	{
-		String x = "Criação da base";
+		x = "Criação da base";
+		y = "Criação da base";
 	}
-	
+	public static void main(String[] args) {
+		System.out.println(x);
+	}
 }

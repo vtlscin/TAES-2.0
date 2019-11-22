2f01a63070100552b3865ebbf95be0bff8f8e14d
Vinicius Thiago <vtls@cin.ufpe.br>
2019-11-22 11:19:07 -0300

    alteraçao pelo left

--- src/blocoEstatico/classeParaConflito.java
+++ src/blocoEstatico/classeParaConflito.java
@@ -4,9 +4,12 @@ public class classeParaConflito {
 
 	static String x;
 	static String y;
+	static String z;
+	
 	static 
 	{
 		x = "Criação da base";
+		z = "Criação do left";
 		y = "Criação da base";
 	}
 	public static void main(String[] args) {

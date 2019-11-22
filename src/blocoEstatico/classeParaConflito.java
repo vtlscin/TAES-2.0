f5c3bef8bdd24b079c8de69ec926af4f75c20a29
Vinicius Thiago <vtls@cin.ufpe.br>
2019-11-22 11:21:57 -0300

    alteracao pelo right

--- src/blocoEstatico/classeParaConflito.java
+++ src/blocoEstatico/classeParaConflito.java
@@ -2,11 +2,14 @@ package blocoEstatico;
 
 public class classeParaConflito {
 
+	static String w;
 	static String x;
 	static String y;
+	
 	static 
 	{
 		x = "Criação da base";
+		w = "Criação do right";
 		y = "Criação da base";
 	}
 	public static void main(String[] args) {
